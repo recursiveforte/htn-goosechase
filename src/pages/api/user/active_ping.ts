1622a65cf9acf301e91c5e11d60ba37d9fcfec98
Cheru Berhanu <cheru@cheru.dev>
2024-09-14 11:54:30 -0400

    fix active_ping.ts

--- src/pages/api/user/active_ping.ts
+++ src/pages/api/user/active_ping.ts
@@ -2,7 +2,7 @@ import { NextApiRequest, NextApiResponse } from 'next'
 import prisma from '@/lib/db/prisma'
 
 type ResponseData = {
-  error?: 'MALFORMED_DATA' | 'INCORRECT_METHOD'
+  error?: "MALFORMED_DATA" | "INCORRECT_METHOD"
   data?: {}
 }
 
@@ -10,25 +10,20 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseData>
 ) {
-  if (req.method !== 'POST') res.status(400).json({ error: 'INCORRECT_METHOD' })
+  if (req.method !== "POST") res.status(400).json({error: 'INCORRECT_METHOD'})
 
-  // const userId: number = req.body.phone
+  const userId: number = req.body.phone
 
-  // if (!userId || !roomId)
-  //   return res.status(400).json({ error: 'MALFORMED_DATA' })
+  if (!userId) return res.status(400).json({error: 'MALFORMED_DATA'})
 
-  // await prisma.user.update({
-  //   where: {
-  //     id: roomId,
-  //   },
-  //   data: {
-  //     users: {
-  //       connect: {
-  //         id: userId,
-  //       },
-  //     },
-  //   },
-  // })
+  await prisma.user.update({
+    where: {
+      id: userId
+    },
+    data: {
+      lastInteractedAt: new Date()
+    }
+  })
 
   return res.status(200).json({})
 }
