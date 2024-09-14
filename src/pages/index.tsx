3e180d74030bb68d559de6adb703f892286d5feb
Faisal Sayed <faisal.sayed502@gmail.com>
2024-09-14 16:27:40 -0400

    feat: animate leaderboard

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,113 +8,278 @@ import parsePhoneNumberFromString from 'libphonenumber-js'
 import { getUserState, setUserState, UserState } from '@/lib/userState'
 import { CreateAccountResponse } from './api/user/createAccount'
 import useSWR from 'swr'
-import {User} from "@prisma/client"
+import { User } from '@prisma/client'
+import { motion } from 'framer-motion'
 
 // @ts-ignore
-const fetcher = (...args: any[]) => fetch(...args).then(res => res.json())
+const fetcher = (...args: any[]) => fetch(...args).then((res) => res.json())
 
 const QrReader = dynamic(() => import('../components/QrReader'), { ssr: false })
 
-function LeaderboardItem({rank, name, score}: {rank: number, name: string, score: number}) {
-  return <div style={{ display: 'flex', width: '100%', gap: '8px', alignItems: 'center', justifyContent: 'center', background: '#ff8c374E', paddingTop: '8px', paddingBottom: '8px', borderRadius: '8px', marginBottom: '8px' }}>
-    <img src="https://preview.redd.it/1k2p6tly25x61.jpg?width=640&crop=smart&auto=webp&s=8f12f643c5f765c9d563c0c4d2bec764ca29469d" style={{ borderRadius: 999, marginLeft: '8px', marginRight: '6px' }} height="36px" width="36px" />
-    <span>#{rank}</span>
-    <span style={{ flexGrow: 1 }}>{name}</span>
-    <span style={{ marginRight: '8px' }}>🪿 {score}</span>
-  </div>
+function LeaderboardItem({
+  rank,
+  name,
+  score,
+}: {
+  rank: number
+  name: string
+  score: number
+}) {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        width: '100%',
+        gap: '8px',
+        alignItems: 'center',
+        justifyContent: 'center',
+        background: '#ff8c374E',
+        paddingTop: '8px',
+        paddingBottom: '8px',
+        borderRadius: '8px',
+        marginBottom: '8px',
+      }}
+    >
+      <img
+        src="https://preview.redd.it/1k2p6tly25x61.jpg?width=640&crop=smart&auto=webp&s=8f12f643c5f765c9d563c0c4d2bec764ca29469d"
+        style={{ borderRadius: 999, marginLeft: '8px', marginRight: '6px' }}
+        height="36px"
+        width="36px"
+      />
+      <span>#{rank}</span>
+      <span style={{ flexGrow: 1 }}>{name}</span>
+      <span style={{ marginRight: '8px' }}>🪿 {score}</span>
+    </div>
+  )
 }
 
-function currentUserOnLeaderboard(leaderboard: (User & { score: number })[], userId: number) {
-  return leaderboard.map((user, i) => ({...user, rank: i + 1})).filter(user => user.id == userId)[0]
+function currentUserOnLeaderboard(
+  leaderboard: (User & { score: number })[],
+  userId: number
+) {
+  return leaderboard
+    .map((user, i) => ({ ...user, rank: i + 1 }))
+    .filter((user) => user.id == userId)[0]
 }
 
-function Game({loginState, loggedIn}: {loginState: any, loggedIn: any}) {
+function Game({ loginState, loggedIn }: { loginState: any; loggedIn: any }) {
   const [scannedCode, setScannedCode] = useState<string | null>(null)
   const [leaderboardOpen, setLeaderboardOpen] = useState<boolean>(false)
-  const { data: leaderboard, error: leaderboardError, isLoading: leaderboardIsLoading } = useSWR('/api/user/get_all', fetcher, { refreshInterval: 1000 })
-  const { data: challenge, error: challengeError, isLoading: challengeIsLoading } = useSWR('/api/challenge/get_current', fetcher, { refreshInterval: 1000 })
-  const { data: user, error: userError, isLoading: userIsLoading } = useSWR(`/api/user/active_ping?userId=${loggedIn ? loggedIn.userId : null}`, fetcher, { refreshInterval: 1000})
+  const {
+    data: leaderboard,
+    error: leaderboardError,
+    isLoading: leaderboardIsLoading,
+  } = useSWR('/api/user/get_all', fetcher, { refreshInterval: 1000 })
+  const {
+    data: challenge,
+    error: challengeError,
+    isLoading: challengeIsLoading,
+  } = useSWR('/api/challenge/get_current', fetcher, { refreshInterval: 1000 })
+  const {
+    data: user,
+    error: userError,
+    isLoading: userIsLoading,
+  } = useSWR(
+    `/api/user/active_ping?userId=${loggedIn ? loggedIn.userId : null}`,
+    fetcher,
+    { refreshInterval: 1000 }
+  )
   return (
-    <div style={{ textAlign: 'center', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
-      { /* @ts-ignore */ }
-      <marquee style={{ fontSize: '24px', width: '100vw', paddingTop: '16px', paddingBottom: '16px', background: '#a633d6' }}>
-        <b><i>The game is being played at dinner! Join us.</i></b> { /* @ts-ignore */}
+    <div
+      style={{
+        textAlign: 'center',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '100vh',
+      }}
+    >
+      {/* @ts-ignore */}
+      <marquee
+        style={{
+          fontSize: '24px',
+          width: '100vw',
+          paddingTop: '16px',
+          paddingBottom: '16px',
+          background: '#a633d6',
+        }}
+      >
+        <b>
+          <i>The game is being played at dinner! Join us.</i>
+        </b>{' '}
+        {/* @ts-ignore */}
       </marquee>
-      {!leaderboardOpen && <>
-        {challenge && <div style={{ width: 'min(500px, 100vw)', height: 'min(500px, 100vw)'}}>
-          <QrReader
-            onScan={async (text) => {
-              const code = text.split('/').slice(-1)[0]
-              if (code.split('-').length !== 4) {
-                return toast.error('Invalid QR code')
-              }
-              if (code !== scannedCode) {
-                setScannedCode(code)
-                toast.success('QR code scanned, your code is: ' + code, {
-                  id: 'scan_self',
-                  duration: 5000,
-                })                
-                toast.promise(
-                  fetch("/api/tag_user", {
-                    method: "POST",
-                    body: JSON.stringify({
-                      taggedBadgeCode: code,
-                      taggerId: loggedIn.userId
+      {!leaderboardOpen && (
+        <>
+          {challenge && (
+            <div
+              style={{
+                width: 'min(500px, 100vw)',
+                height: 'min(500px, 100vw)',
+              }}
+            >
+              <QrReader
+                onScan={async (text) => {
+                  const code = text.split('/').slice(-1)[0]
+                  if (code.split('-').length !== 4) {
+                    return toast.error('Invalid QR code')
+                  }
+                  if (code !== scannedCode) {
+                    setScannedCode(code)
+                    toast.success('QR code scanned, your code is: ' + code, {
+                      id: 'scan_self',
+                      duration: 5000,
                     })
-                  }),
-                   {
-                     loading: 'Tagging...',
-                     success: <b>Tagged! Congratulations!</b>,
-                     error: <b>Looks like that wasn't the target, sorry,</b>,
-                   }
-                 );
-              }
-            }}
-          />
-        </div>}
-        {challenge && <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', flexGrow: 1 }}>
-          <h3 style={{ margin: 0, marginTop: '16px' }}>
-            The current target is:
-          </h3>
-          <h1 style={{ margin: 0, marginTop: '16px' }}>
-            {challenge.tagged.name}
-          </h1>
-          <h3 style={{ margin: '16px', fontWeight: 400 }}>Find them in the room, scan their code, and you'll both earn points.</h3>
-        </div>}
-        {!challenge && <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', flexGrow: 1 }}>
-            <h1 style={{ margin: 0, marginTop: '16px', color: '#FF69B4' }}>
-              <i>There's no challenge at the moment! Keep an eye on your phone for any texts...</i>
-            </h1>
-          </div>}
-      </>}
-      {leaderboardOpen && <>
-        <div style={{ flexGrow: 1, textAlign: 'left', width: 'calc(100% - 32px)', flexShrink: 1, overflowY: 'scroll', position: 'relative', height: '80vh' }}>
+                    toast.promise(
+                      fetch('/api/tag_user', {
+                        method: 'POST',
+                        body: JSON.stringify({
+                          taggedBadgeCode: code,
+                          taggerId: loggedIn.userId,
+                        }),
+                      }),
+                      {
+                        loading: 'Tagging...',
+                        success: <b>Tagged! Congratulations!</b>,
+                        error: <b>Looks like that wasn't the target, sorry,</b>,
+                      }
+                    )
+                  }
+                }}
+              />
+            </div>
+          )}
+          {challenge && (
+            <div
+              style={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                justifyContent: 'center',
+                flexGrow: 1,
+              }}
+            >
+              <h3 style={{ margin: 0, marginTop: '16px' }}>
+                The current target is:
+              </h3>
+              <h1 style={{ margin: 0, marginTop: '16px' }}>
+                {challenge.tagged.name}
+              </h1>
+              <h3 style={{ margin: '16px', fontWeight: 400 }}>
+                Find them in the room, scan their code, and you'll both earn
+                points.
+              </h3>
+            </div>
+          )}
+          {!challenge && (
+            <div
+              style={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                justifyContent: 'center',
+                flexGrow: 1,
+              }}
+            >
+              <h1 style={{ margin: 0, marginTop: '16px', color: '#FF69B4' }}>
+                <i>
+                  There's no challenge at the moment! Keep an eye on your phone
+                  for any texts...
+                </i>
+              </h1>
+            </div>
+          )}
+        </>
+      )}
+
+      {leaderboardOpen && (
+        <motion.div
+          initial={{ opacity: 0, y: 1000 }}
+          animate={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 0, y: 100 }}
+          transition={{ duration: 0.15 }}
+          style={{
+            flexGrow: 1,
+            textAlign: 'left',
+            width: 'calc(100% - 32px)',
+            flexShrink: 1,
+            overflowY: 'scroll',
+            position: 'relative',
+            height: '80vh',
+          }}
+        >
           <h1 style={{ margin: 0, width: '100%', marginTop: '16px' }}>
             <u>Goose Chase Leaderboard</u>
           </h1>
           <p style={{ textAlign: 'left', width: '100%' }}>
-            Looking to earn points? Keep an eye on your texts for new challenges and be the first to tag the target. <i>(Or get lucky and be the target!)</i> 
+            Looking to earn points? Keep an eye on your texts for new challenges
+            and be the first to tag the target.{' '}
+            <i>(Or get lucky and be the target!)</i>
           </p>
-          {leaderboard.map((user : any, i: number) => (
-            <LeaderboardItem rank={i+1} name={user.badgeCode} score={user.score} />
+          {leaderboard.map((user: any, i: number) => (
+            <LeaderboardItem
+              rank={i + 1}
+              name={user.badgeCode}
+              score={user.score}
+            />
           ))}
           <br />
-        </div>
-      </>}
-      <div style={{ background: '#338eda', paddingTop: '16px', paddingBottom: '16px', width: '100%', display: 'flex', alignItems: 'center', gap: '16px' }}>
-        <img src="https://preview.redd.it/1k2p6tly25x61.jpg?width=640&crop=smart&auto=webp&s=8f12f643c5f765c9d563c0c4d2bec764ca29469d" style={{ borderRadius: 999, marginLeft: '16px' }} height="36px" width="36px" />
-        <span style={{ flexGrow: 1, textAlign: 'left' }}>{user?.badgeCode || "Loading..."}</span>
-        <span style={{ background: 'rgba(0, 0, 0, 0.3)', width: '64px', borderRadius: '48px', padding: '4px 8px' }} onClick={() => setLeaderboardOpen(!leaderboardOpen)}>
-          #{leaderboard &&  currentUserOnLeaderboard(leaderboard, loggedIn.userId).rank || "?"}
+        </motion.div>
+      )}
+      <div
+        onClick={() => setLeaderboardOpen(!leaderboardOpen)}
+        style={{
+          background: '#338eda',
+          paddingTop: '16px',
+          paddingBottom: '16px',
+          width: '100%',
+          display: 'flex',
+          alignItems: 'center',
+          gap: '16px',
+          cursor: 'pointer',
+        }}
+      >
+        <img
+          src="https://preview.redd.it/1k2p6tly25x61.jpg?width=640&crop=smart&auto=webp&s=8f12f643c5f765c9d563c0c4d2bec764ca29469d"
+          style={{ borderRadius: 999, marginLeft: '16px' }}
+          height="36px"
+          width="36px"
+        />
+        <span style={{ flexGrow: 1, textAlign: 'left' }}>
+          {user?.badgeCode || 'Loading...'}
         </span>
-        <span style={{ background: 'rgba(0, 0, 0, 0.3)', width: '64px', borderRadius: '48px', padding: '4px 8px', marginRight: '16px' }} onClick={() => setLeaderboardOpen(!leaderboardOpen)}>
-          🪿 {leaderboard && currentUserOnLeaderboard(leaderboard, loggedIn.userId).score || 0}  
+        <span
+          style={{
+            background: 'rgba(0, 0, 0, 0.3)',
+            width: '64px',
+            borderRadius: '48px',
+            padding: '4px 8px',
+          }}
+        >
+          #
+          {(leaderboard &&
+            currentUserOnLeaderboard(leaderboard, loggedIn.userId).rank) ||
+            '?'}
+        </span>
+        <span
+          style={{
+            background: 'rgba(0, 0, 0, 0.3)',
+            width: '64px',
+            borderRadius: '48px',
+            padding: '4px 8px',
+            marginRight: '16px',
+          }}
+        >
+          🪿{' '}
+          {(leaderboard &&
+            currentUserOnLeaderboard(leaderboard, loggedIn.userId).score) ||
+            0}
         </span>
       </div>
     </div>
   )
 }
-
 
 type LoginState =
   | { kind: 'qrScanner' }
