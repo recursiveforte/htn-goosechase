388d846f0b55d6e7c4bc679907b384539194b211
Cheru Berhanu <cheru@cheru.dev>
2024-09-14 11:20:41 -0400

    new (untested) api

---
+++ src/pages/api/tag_user.ts
@@ -0,0 +1,52 @@
+import prisma from "../../lib/db/prisma";
+import { NextApiRequest, NextApiResponse } from 'next'
+import { getCurrentChallenge } from '@/lib/db/challenge'
+
+type ResponseData = {
+    error?: "INCORRECT_TAGGEE" | "INCORRECT_METHOD" | "MALFORMED_DATA" | "NO_CHALLENGE"
+    message?: "ok"
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseData>
+) {
+    if (req.method !== "POST") res.status(400).json({error: 'INCORRECT_METHOD'})
+
+    const data = await req.body;
+    const taggeeUsername: string = data.taggeeUsername
+    const taggerId: number = data.taggerId
+
+    if (!taggeeUsername || !taggerId) res.status(400).json({error: 'MALFORMED_DATA'})
+
+    const currentChallenge = await getCurrentChallenge()
+
+    if (!currentChallenge) return res.status(400).json({error: 'NO_CHALLENGE'})
+
+    if (currentChallenge.taggee.username != taggeeUsername) {
+        res.status(400).json({ error: "INCORRECT_TAGGEE" })
+    }
+
+    const taggee = await prisma.user.findUnique({
+        where: {
+            username: taggeeUsername
+        }
+    })
+
+    if (taggee?.id == taggerId) {
+        res.status(400).json({ error: "INCORRECT_TAGGEE" })
+    }
+
+    await prisma.challenge.update({
+        where: {
+            id: currentChallenge.id
+        },
+        data: {
+            taggerId: taggerId,
+        }
+    })
+
+    // TODO: logic to start new challenge
+
+    res.status(200).json({})
+}
