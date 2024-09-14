384815e2065ca511ada583af942d5d86b5beff31
Lexi Mattick <lexi.mattick@gmail.com>
2024-09-14 13:44:43 -0400

    unfuck

--- src/lib/db/challenge.ts
+++ src/lib/db/challenge.ts
@@ -12,7 +12,7 @@ export const getCurrentChallenge = () =>
       id: 'desc',
     },
     include: {
-      taggee: true,
+      // taggee: true,
       tagger: true,
     },
   })
--- src/pages/api/tag_user.ts
+++ src/pages/api/tag_user.ts
@@ -17,40 +17,40 @@ export default async function handler(
 ) {
   if (req.method !== 'POST') res.status(400).json({ error: 'INCORRECT_METHOD' })
 
-  const data = await req.body
-  const taggeeBadgeCode: string = data.taggeeBadgeCode
-  const taggerId: number = data.taggerId
+  // const data = await req.body
+  // const taggeeBadgeCode: string = data.taggeeBadgeCode
+  // const taggerId: number = data.taggerId
 
-  if (!taggeeBadgeCode || !taggerId)
-    res.status(400).json({ error: 'MALFORMED_DATA' })
+  // if (!taggeeBadgeCode || !taggerId)
+  //   res.status(400).json({ error: 'MALFORMED_DATA' })
 
-  const currentChallenge = await getCurrentChallenge()
+  // const currentChallenge = await getCurrentChallenge()
 
-  if (!currentChallenge) return res.status(400).json({ error: 'NO_CHALLENGE' })
+  // if (!currentChallenge) return res.status(400).json({ error: 'NO_CHALLENGE' })
 
-  if (currentChallenge.taggee.badgeCode != taggeeBadgeCode) {
-    res.status(400).json({ error: 'INCORRECT_TAGGEE' })
-  }
+  // if (currentChallenge.taggee.badgeCode != taggeeBadgeCode) {
+  //   res.status(400).json({ error: 'INCORRECT_TAGGEE' })
+  // }
 
-  const taggee = await prisma.user.findUnique({
-    where: {
-      badgeCode: taggeeBadgeCode,
-    },
-  })
+  // const taggee = await prisma.user.findUnique({
+  //   where: {
+  //     badgeCode: taggeeBadgeCode,
+  //   },
+  // })
 
-  if (taggee?.id == taggerId) {
-    res.status(400).json({ error: 'INCORRECT_TAGGEE' })
-  }
+  // if (taggee?.id == taggerId) {
+  //   res.status(400).json({ error: 'INCORRECT_TAGGEE' })
+  // }
 
-  await prisma.challenge.update({
-    where: {
-      id: currentChallenge.id,
-    },
-    data: {
-      taggerId: taggerId,
-      taggedAt: new Date(),
-    },
-  })
+  // await prisma.challenge.update({
+  //   where: {
+  //     id: currentChallenge.id,
+  //   },
+  //   data: {
+  //     taggerId: taggerId,
+  //     taggedAt: new Date(),
+  //   },
+  // })
 
   // TODO: logic to start new challenge
 
