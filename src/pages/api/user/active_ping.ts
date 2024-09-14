7f874688686538f1653d1dc929cbd9c8b5925bd9
Cheru Berhanu <cheru@cheru.dev>
2024-09-14 11:48:45 -0400

    active ping

---
+++ src/pages/api/user/active_ping.ts
@@ -0,0 +1,30 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import prisma from '@/lib/db/prisma'
+
+type ResponseData = {
+  error?: "MALFORMED_DATA" | "INCORRECT_METHOD"
+  data?: {}
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseData>
+) {
+  if (req.method !== "POST") res.status(400).json({error: 'INCORRECT_METHOD'})
+
+  const data = req.body
+  const userId: number = req.body.phone
+
+  if (!userId) return res.status(400).json({error: 'MALFORMED_DATA'})
+
+  await prisma.user.update({
+    where: {
+      id: userId
+    },
+    data: {
+      lastInteractedAt: new Date()
+    }
+  })
+
+  return res.status(200).json({})
+}
